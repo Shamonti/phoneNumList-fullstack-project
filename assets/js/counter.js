4fe0a336f56a093079263a297c9c6de1e4ec9267
Shamonti <67649277+Shamonti@users.noreply.github.com>
2022-01-30 15:52:29 +0600

    renamed counter

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,24 +1,53 @@
 //START COUNTER ON HOVER OVER SECTION
-const sectionCounter = document.querySelector('.section-counter');
+// const sectionCounter = document.querySelector('.section-counter');
 
-sectionCounter.addEventListener('mouseover', function () {
-  //COUNT
-  $(document).ready(function () {
+// sectionCounter.addEventListener('mouseover', function () {
+//   //COUNT
+//   $(document).ready(function () {
+//     $('.counter').each(function () {
+//       $(this)
+//         .prop('Counter', 0)
+//         .animate(
+//           { Counter: $(this).text() },
+//           {
+//             duration: 4000,
+//             easing: 'swing',
+//             step: function (now) {
+//               $(this).text(Math.ceil(now));
+//             },
+//           }
+//         );
+//     });
+//   });
+// });
+
+let a = 0;
+$(window).scroll(function () {
+  let oTop = $('.section-counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
     $('.counter').each(function () {
-      $(this)
-        .prop('Counter', 0)
-        .animate(
-          {
-            Counter: $(this).text(),
+      let $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text(),
+      }).animate(
+        {
+          countNum: countTo,
+        },
+
+        {
+          duration: 7000,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
           },
-          {
-            duration: 2000,
-            easing: 'swing',
-            step: function (now) {
-              $(this).text(Math.ceil(now));
-            },
-          }
-        );
+          complete: function () {
+            $this.text(this.countNum);
+            //alert('finished');
+          },
+        }
+      );
     });
-  });
+    a = 1;
+  }
 });
