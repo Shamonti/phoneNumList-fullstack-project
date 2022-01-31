3c16988e5383e56d846c55f9f3d73babf44feafe
Shamonti <67649277+Shamonti@users.noreply.github.com>
2022-01-31 11:45:57 +0600

    added meta tags

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,25 +1,4 @@
 //START COUNTER ON HOVER OVER SECTION
-// const sectionCounter = document.querySelector('.section-counter');
-
-// sectionCounter.addEventListener('mouseover', function () {
-//   //COUNT
-//   $(document).ready(function () {
-//     $('.counter').each(function () {
-//       $(this)
-//         .prop('Counter', 0)
-//         .animate(
-//           { Counter: $(this).text() },
-//           {
-//             duration: 4000,
-//             easing: 'swing',
-//             step: function (now) {
-//               $(this).text(Math.ceil(now));
-//             },
-//           }
-//         );
-//     });
-//   });
-// });
 
 let a = 0;
 $(window).scroll(function () {
