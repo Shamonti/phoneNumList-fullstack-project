47e794edd7b319d7ce8e4944ac10682e52f56427
Shamonti <67649277+Shamonti@users.noreply.github.com>
2022-02-19 17:47:11 +0600

    added exports and manage plan page

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -1,8 +1,8 @@
 function doGTranslate(lang_pair) {
   if (lang_pair.value) lang_pair = lang_pair.value;
   if (lang_pair == '') return;
-  var lang = lang_pair.split('|')[1];
-  var plang = location.pathname.split('/')[1];
+  let lang = lang_pair.split('|')[1];
+  let plang = location.pathname.split('/')[1];
   if (
     plang.length != 2 &&
     plang.toLowerCase() != 'zh-cn' &&
