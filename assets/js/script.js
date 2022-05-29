228132d434c413a1bbfd0a61ef409875a98e289e
Shamonti <67649277+Shamonti@users.noreply.github.com>
2022-05-29 17:51:38 +0600

    added new user

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ function readMore() {
 
 //SHOW PASSWORD
 function showPassword() {
-  var x = document.getElementById('password');
+  let x = document.getElementById('password');
   if (x.type === 'password') {
     x.type = 'text';
   } else {
@@ -28,34 +28,34 @@ function showPassword() {
 }
 
 //DATE PICKER
-$(function () {
-  var from_$input = $('#input_from').pickadate(),
-    from_picker = from_$input.pickadate('picker');
+// $(function () {
+//   var from_$input = $('#input_from').pickadate(),
+//     from_picker = from_$input.pickadate('picker');
 
-  var to_$input = $('#input_to').pickadate(),
-    to_picker = to_$input.pickadate('picker');
+//   var to_$input = $('#input_to').pickadate(),
+//     to_picker = to_$input.pickadate('picker');
 
-  // Check if there’s a “from” or “to” date to start with.
-  if (from_picker.get('value')) {
-    to_picker.set('min', from_picker.get('select'));
-  }
-  if (to_picker.get('value')) {
-    from_picker.set('max', to_picker.get('select'));
-  }
+//   // Check if there’s a “from” or “to” date to start with.
+//   if (from_picker.get('value')) {
+//     to_picker.set('min', from_picker.get('select'));
+//   }
+//   if (to_picker.get('value')) {
+//     from_picker.set('max', to_picker.get('select'));
+//   }
 
-  // When something is selected, update the “from” and “to” limits.
-  from_picker.on('set', function (event) {
-    if (event.select) {
-      to_picker.set('min', from_picker.get('select'));
-    } else if ('clear' in event) {
-      to_picker.set('min', false);
-    }
-  });
-  to_picker.on('set', function (event) {
-    if (event.select) {
-      from_picker.set('max', to_picker.get('select'));
-    } else if ('clear' in event) {
-      from_picker.set('max', false);
-    }
-  });
-});
+//   // When something is selected, update the “from” and “to” limits.
+//   from_picker.on('set', function (event) {
+//     if (event.select) {
+//       to_picker.set('min', from_picker.get('select'));
+//     } else if ('clear' in event) {
+//       to_picker.set('min', false);
+//     }
+//   });
+//   to_picker.on('set', function (event) {
+//     if (event.select) {
+//       from_picker.set('max', to_picker.get('select'));
+//     } else if ('clear' in event) {
+//       from_picker.set('max', false);
+//     }
+//   });
+// });
